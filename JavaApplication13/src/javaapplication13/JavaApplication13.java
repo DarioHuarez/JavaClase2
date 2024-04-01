404d221f05b6153638e7c3dd9a1a267ac9a0af92
dario <dario@DESKTOP-98PQLQ5>
2024-03-31 19:47:48 -0500

    Primer Ejercicio

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -14,7 +14,24 @@ public class JavaApplication13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+       final float PI = 3.141592f; 
+      
+       //datos cilindro 1
+       float radioCilindro1 = 4.0f;
+       float alturaCilindro1 = 2.5f;
+       float areaCilindro1;
+       
+       // datos cilindro 2
+       float radioCilindro2 = 2.0f;
+       float alturaCilindro2 = 5.0f;
+       float areaCilindro2;
+       
+       areaCilindro1 = 2.0f*PI*(radioCilindro1*alturaCilindro1 + (float)Math.pow(radioCilindro1,2.0));
+       areaCilindro2 = 2.0f*PI*(radioCilindro2*alturaCilindro2 + (float)Math.pow(radioCilindro2,2.0));
+       
+        System.out.println("El area del cilindro 1 es: "+areaCilindro1 + " metros cuadrados");
+        System.out.println("El area del cilindro 2 es "+areaCilindro2 + " metros cuadrados");
     }
     
 }
