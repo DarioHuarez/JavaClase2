f9dd0bd81251321ad2f9c6dec0622b522c0d21f7
dario <dario@DESKTOP-98PQLQ5>
2024-04-04 02:20:28 -0500

    Octavo ejercicio

---
+++ JavaApplication27/src/javaapplication27/JavaApplication27.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication27;
+
+/**
+ *
+ * @author dario
+ */
+public class JavaApplication27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final float PI = 3.141592f;
+        float radio = 8.0f;
+        float area;
+        
+        area = PI*(float)Math.pow(radio,2.0);
+        System.out.println("El area es:" +area);
+        
+    }
+    
+}
